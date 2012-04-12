3476c6aec709d08f2ec31186e05275cc57c838dc
lovenew <lovenew@lovenew-PC>
2012-04-12 18:03:31 +0800

    4-12

--- gen/cn/whzxt/android/R.java
+++ gen/cn/whzxt/android/R.java
@@ -29,30 +29,39 @@ public final class R {
         public static final int transparent=0x7f020006;
     }
     public static final class id {
-        public static final int btnCoachLogout=0x7f06000c;
+        public static final int btnCoachLogout=0x7f06000e;
         public static final int imgCoach=0x7f06000b;
+        public static final int imgStudent=0x7f060013;
+        public static final int layCoachInfo=0x7f06000a;
         public static final int layCoachTitle=0x7f060007;
+        public static final int layStudentInfo=0x7f060012;
+        public static final int layStudentTitle=0x7f06000f;
         public static final int tableRow1=0x7f060000;
         public static final int tableRow2=0x7f060004;
         public static final int tableRow3=0x7f060005;
         public static final int tableRow4=0x7f060006;
-        public static final int tableRow5=0x7f060011;
-        public static final int tableRow6=0x7f060015;
-        public static final int tableRow7=0x7f060016;
-        public static final int tableRow8=0x7f060017;
+        public static final int tableRow5=0x7f06001a;
+        public static final int tableRow6=0x7f06001e;
+        public static final int tableRow7=0x7f06001f;
+        public static final int tableRow8=0x7f060020;
         public static final int textView1=0x7f060008;
-        public static final int textView12=0x7f060012;
-        public static final int textView2=0x7f06000d;
-        public static final int textView5=0x7f060014;
-        public static final int textView6=0x7f060013;
-        public static final int txtCoachName=0x7f06000a;
+        public static final int textView12=0x7f06001b;
+        public static final int textView2=0x7f060010;
+        public static final int textView5=0x7f06001d;
+        public static final int textView6=0x7f06001c;
+        public static final int txtCoachCard=0x7f06000d;
+        public static final int txtCoachName=0x7f06000c;
         public static final int txtDeviceName=0x7f060002;
         public static final int txtInfoCoach=0x7f060009;
-        public static final int txtInfoStudent=0x7f06000e;
-        public static final int txtLngLat=0x7f060010;
+        public static final int txtInfoStudent=0x7f060011;
+        public static final int txtLngLat=0x7f060019;
         public static final int txtSchoolName=0x7f060001;
-        public static final int txtStatus=0x7f06000f;
+        public static final int txtStartTime=0x7f060016;
+        public static final int txtStatus=0x7f060018;
+        public static final int txtStudentCard=0x7f060015;
+        public static final int txtStudentName=0x7f060014;
         public static final int txtSystemTime=0x7f060003;
+        public static final int txtTrainTime=0x7f060017;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/cn/whzxt/android/MainActivity.java
+++ src/cn/whzxt/android/MainActivity.java
@@ -18,12 +18,12 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 
-import android.R.bool;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
@@ -32,6 +32,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -39,22 +40,27 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private TextView txtSchoolName, txtDeviceName, txtSystemTime;
-	private TextView txtInfoCoach, txtInfoStudent, txtCard, txtStatus, txtLngLat;
-	private TextView txtCoachName;
-	private ImageView imgCoach;
-	private LinearLayout layCoachTitle;
+	private TextView txtInfoCoach, txtInfoStudent, txtStatus, txtLngLat;
+	private TextView txtCoachName, txtCoachCard, txtStudentName, txtStudentCard;
+	private TextView txtStartTime, txtTrainTime;
+	private ImageView imgCoach, imgStudent;
+	private LinearLayout layCoachTitle, layStudentTitle, layCoachInfo, layStudentInfo;
 	private Button btnCoachLogout;
-	private String deviceID, deviceName, schoolName, session;
+	private String deviceID, deviceName, schoolID, schoolName, session;
 	private String server;
-	private String uploadresult;
+	private String uploadresult, cardresult;
+	private String coachID = "", coachName, coachCard, studentID = "", studentCard, studentName;
 	private int col = 0;
+	private Date startTime;
 	private double lng, lat;
 	private SimpleDateFormat dateFormat, timeFormat;
 	private LocationManager locationManager;
 	private Timer _timerUpload, _timerFlicker, _timerSecond;
+	private SharedPreferences settings;
 
 	private static final String PATH = "/dev/s3c2410_serial1";
 	private static final int BAUD = 9600;
@@ -93,26 +99,45 @@ public class MainActivity extends Activity {
 		txtInfoCoach = (TextView) findViewById(R.id.txtInfoCoach);
 		txtInfoStudent = (TextView) findViewById(R.id.txtInfoStudent);
 		layCoachTitle = (LinearLayout) findViewById(R.id.layCoachTitle);
+		layStudentTitle = (LinearLayout) findViewById(R.id.layStudentTitle);
+		layCoachInfo = (LinearLayout) findViewById(R.id.layCoachInfo);
+		layStudentInfo = (LinearLayout) findViewById(R.id.layStudentInfo);
 		txtCoachName = (TextView) findViewById(R.id.txtCoachName);
+		txtCoachCard = (TextView) findViewById(R.id.txtCoachCard);
+		txtStudentName = (TextView) findViewById(R.id.txtStudentName);
+		txtStudentCard = (TextView) findViewById(R.id.txtStudentCard);
 		imgCoach = (ImageView) findViewById(R.id.imgCoach);
+		imgStudent = (ImageView) findViewById(R.id.imgStudent);
+		txtStartTime = (TextView) findViewById(R.id.txtStartTime);
+		txtTrainTime = (TextView) findViewById(R.id.txtTrainTime);
 		txtStatus = (TextView) findViewById(R.id.txtStatus);
 		txtLngLat = (TextView) findViewById(R.id.txtLngLat);
 		btnCoachLogout = (Button) findViewById(R.id.btnCoachLogout);
 		btnCoachLogout.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				showCoach(false);
+				if (txtInfoStudent.getVisibility() == View.VISIBLE) {
+					coachID = "";
+					coachName = "";
+					coachCard = "";
+					showCoach(false);
+				} else {
+					Toast.makeText(MainActivity.this, "请先取出学员卡", Toast.LENGTH_LONG).show();
+				}
 			}
 		});
 		Bundle bundle = this.getIntent().getExtras();
 		deviceID = bundle.getString("deviceID");
 		deviceName = bundle.getString("deviceName");
+		schoolID = "11111111";
 		schoolName = bundle.getString("schoolName");
 		session = bundle.getString("session");
 		txtSchoolName.setText(schoolName);
 		txtDeviceName.setText(deviceName);
 		dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		timeFormat = new SimpleDateFormat("HH:mm");
+		timeFormat = new SimpleDateFormat("HH:mm:ss");
 		txtSystemTime.setText(dateFormat.format(new Date()));
+
+		settings = getSharedPreferences("whzxt.net", 0);
 
 		server = getString(R.string.server1);
 
@@ -173,26 +198,41 @@ public class MainActivity extends Activity {
 				handler.sendMessage(msg);
 			}
 		}, 1000, 1000);
+		// 显示教练信息
+		if (settings.getString("coachID", "") != "") {
+			coachID = settings.getString("coachID", "");
+			coachName = settings.getString("coachName", "");
+			coachCard = settings.getString("coachCard", "");
+			showCoach(true);
+		}
 	}
 
 	// 文字闪烁
 	private void flicker() {
 		switch (col) {
 		case 0:
-			txtInfoCoach.setTextColor(Color.TRANSPARENT);
-			txtInfoStudent.setTextColor(Color.TRANSPARENT);
+			if (txtInfoCoach.getVisibility() == View.VISIBLE)
+				txtInfoCoach.setTextColor(Color.TRANSPARENT);
+			if (txtInfoStudent.getVisibility() == View.VISIBLE)
+				txtInfoStudent.setTextColor(Color.TRANSPARENT);
 			break;
 		case 1:
-			txtInfoCoach.setTextColor(Color.YELLOW);
-			txtInfoStudent.setTextColor(Color.YELLOW);
+			if (txtInfoCoach.getVisibility() == View.VISIBLE)
+				txtInfoCoach.setTextColor(Color.YELLOW);
+			if (txtInfoStudent.getVisibility() == View.VISIBLE)
+				txtInfoStudent.setTextColor(Color.YELLOW);
 			break;
 		case 2:
-			txtInfoCoach.setTextColor(Color.RED);
-			txtInfoStudent.setTextColor(Color.RED);
+			if (txtInfoCoach.getVisibility() == View.VISIBLE)
+				txtInfoCoach.setTextColor(Color.RED);
+			if (txtInfoStudent.getVisibility() == View.VISIBLE)
+				txtInfoStudent.setTextColor(Color.RED);
 			break;
 		case 3:
-			txtInfoCoach.setTextColor(Color.BLUE);
-			txtInfoStudent.setTextColor(Color.BLUE);
+			if (txtInfoCoach.getVisibility() == View.VISIBLE)
+				txtInfoCoach.setTextColor(Color.BLUE);
+			if (txtInfoStudent.getVisibility() == View.VISIBLE)
+				txtInfoStudent.setTextColor(Color.BLUE);
 			break;
 		default:
 			break;
@@ -207,11 +247,89 @@ public class MainActivity extends Activity {
 		txtSystemTime.setText(dateFormat.format(new Date()));
 		if (fd > 0) {
 			if (Native.chk_4442(fd) == 0) {
-				if (txtCoachName.getVisibility() == View.GONE) {
-					showCoach(true);
+				if (txtInfoCoach.getVisibility() == View.VISIBLE) {
+					// String data = Native.srd_4442(fd, 0x00, 0xff);
+					coachCard = "00100005";
+					new Thread() {
+						public void run() {
+							HttpPost httpRequest = new HttpPost(server + "/getcoachinfo.ashx");
+							List<NameValuePair> params = new ArrayList<NameValuePair>(4);
+							params.add(new BasicNameValuePair("deviceid", deviceID)); // 设备号码
+							params.add(new BasicNameValuePair("session", session)); // 当前会话
+							params.add(new BasicNameValuePair("school", schoolID)); // 驾校ID
+							params.add(new BasicNameValuePair("card", coachCard)); // 卡号
+							try {
+								httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+								HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
+								if (httpResponse.getStatusLine().getStatusCode() == 200) {
+									cardresult = EntityUtils.toString(httpResponse.getEntity());
+									handler.post(new Runnable() {
+										public void run() {
+											if (cardresult.startsWith("s|")) {
+												String[] results = cardresult.split("\\|");
+												coachID = results[1];
+												coachName = results[2];
+												showCoach(true);
+											} else {
+												Toast.makeText(MainActivity.this, cardresult.split("\\|")[1], Toast.LENGTH_LONG).show();
+											}
+										}
+									});
+								}
+							} catch (Exception e) {
+								e.printStackTrace();
+								Toast.makeText(MainActivity.this, "网络错误,暂时无法获取教练信息,请尝试重新插卡", Toast.LENGTH_LONG).show();
+								changeServer();
+							}
+						}
+					}.start();
 				}
 			} else if (Native.chk_102(fd) == 0) {
 				// 102卡
+				studentCard = "00100491";
+				if (txtInfoCoach.getVisibility() == View.GONE && txtInfoStudent.getVisibility() == View.VISIBLE) {
+					new Thread() {
+						public void run() {
+							HttpPost httpRequest = new HttpPost(server + "/getstudentinfo.ashx");
+							List<NameValuePair> params = new ArrayList<NameValuePair>(4);
+							params.add(new BasicNameValuePair("deviceid", deviceID)); // 设备号码
+							params.add(new BasicNameValuePair("session", session)); // 当前会话
+							params.add(new BasicNameValuePair("school", schoolID)); // 驾校ID
+							params.add(new BasicNameValuePair("card", studentCard)); // 学员卡号
+							try {
+								httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+								HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
+								if (httpResponse.getStatusLine().getStatusCode() == 200) {
+									cardresult = EntityUtils.toString(httpResponse.getEntity());
+									handler.post(new Runnable() {
+										public void run() {
+											if (cardresult.startsWith("s|")) {
+												String[] results = cardresult.split("\\|");
+												studentID = results[1];
+												studentName = results[2];
+												startTime = new Date();
+												showStudent(true);
+											} else {
+												Toast.makeText(MainActivity.this, cardresult.split("\\|")[1], Toast.LENGTH_LONG).show();
+											}
+										}
+									});
+								}
+							} catch (Exception e) {
+								e.printStackTrace();
+								Toast.makeText(MainActivity.this, "网络错误,暂时无法获取学员信息,请尝试重新插卡", Toast.LENGTH_LONG).show();
+								changeServer();
+							}
+						}
+					}.start();
+				}
+			} else {
+				if (txtInfoStudent.getVisibility() == View.GONE) {
+					studentID = "";
+					studentName = "";
+					studentCard = "";
+					showStudent(false);
+				}
 			}
 		}
 	}
@@ -221,11 +339,13 @@ public class MainActivity extends Activity {
 		new Thread() {
 			public void run() {
 				HttpPost httpRequest = new HttpPost(server + "/upload.ashx");
-				List<NameValuePair> params = new ArrayList<NameValuePair>(4);
-				params.add(new BasicNameValuePair("deviceid", deviceID));
-				params.add(new BasicNameValuePair("session", session));
-				params.add(new BasicNameValuePair("lng", Double.toString(lng)));
-				params.add(new BasicNameValuePair("lat", Double.toString(lat)));
+				List<NameValuePair> params = new ArrayList<NameValuePair>(6);
+				params.add(new BasicNameValuePair("deviceid", deviceID)); // 设备号码
+				params.add(new BasicNameValuePair("session", session)); // 当前会话
+				params.add(new BasicNameValuePair("lng", Double.toString(lng))); // 经度
+				params.add(new BasicNameValuePair("lat", Double.toString(lat))); // 纬度
+				params.add(new BasicNameValuePair("coach", coachID)); // 教练
+				params.add(new BasicNameValuePair("student", studentID)); // 学员
 				try {
 					httpRequest.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
 					HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequest);
@@ -241,33 +361,58 @@ public class MainActivity extends Activity {
 							}
 						});
 					}
-				} catch (ClientProtocolException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
 				} catch (Exception e) {
 					e.printStackTrace();
+					txtStatus.setText("网络异常[" + getNowTime() + "]");
+					changeServer();
 				}
 			}
 		}.start();
 	}
 
+	// 显示(隐藏)教练信息
 	private void showCoach(Boolean b) {
 		if (b) {
 			layCoachTitle.setBackgroundResource(R.drawable.bg2);
 			txtInfoStudent.setText("请插学员卡");
-			txtCoachName.setText("黄灿");
-			txtCoachName.setVisibility(View.VISIBLE);
-			imgCoach.setVisibility(View.VISIBLE);
+			txtCoachName.setText(coachName);
+			txtCoachCard.setText(coachCard);
+			layCoachInfo.setVisibility(View.VISIBLE);
 			btnCoachLogout.setVisibility(View.VISIBLE);
 			txtInfoCoach.setVisibility(View.GONE);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putString("coachID", coachID);
+			editor.putString("coachCard", coachCard);
+			editor.putString("coachName", coachName);
+			editor.commit();
+			Toast.makeText(MainActivity.this, "教练登签成功,现在可以取出教练卡", Toast.LENGTH_LONG).show();
 		} else {
 			layCoachTitle.setBackgroundResource(R.drawable.bg1);
 			txtInfoStudent.setText("请先插教练卡");
-			txtCoachName.setVisibility(View.GONE);
-			imgCoach.setVisibility(View.GONE);
+			layCoachInfo.setVisibility(View.GONE);
 			btnCoachLogout.setVisibility(View.GONE);
 			txtInfoCoach.setVisibility(View.VISIBLE);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putString("coachID", "");
+			editor.putString("coachCard", "");
+			editor.putString("coachName", "");
+			editor.commit();
+		}
+	}
+
+	// 显示(隐藏)学员信息
+	private void showStudent(Boolean b) {
+		if (b) {
+			layStudentTitle.setBackgroundResource(R.drawable.bg2);
+			txtStudentName.setText(studentName);
+			txtStudentCard.setText(studentCard);
+			layStudentInfo.setVisibility(View.VISIBLE);
+			txtInfoStudent.setVisibility(View.GONE);
+			txtStartTime.setText("训练开始时间:" + timeFormat.format(startTime));
+		} else {
+			layStudentTitle.setBackgroundResource(R.drawable.bg1);
+			layStudentInfo.setVisibility(View.GONE);
+			txtInfoStudent.setVisibility(View.VISIBLE);
 		}
 	}
 
@@ -282,6 +427,14 @@ public class MainActivity extends Activity {
 
 	private String getNowTime() {
 		return timeFormat.format(new Date());
+	}
+
+	private void changeServer() {
+		if (server.equals(getString(R.string.server1))) {
+			server = getString(R.string.server2);
+		} else {
+			server = getString(R.string.server1);
+		}
 	}
 
 	@Override
